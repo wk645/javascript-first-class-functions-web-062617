7e314e7ce793088c756323007cce12091e526bbd
wk645 <wk645@nyu.edu>
2017-08-08 16:20:12 -0400

    first class

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+	return callback()
+}
+
+function returnsANamedFunction() {
+	return function name(){}
+}
+
+function returnsAnAnonymousFunction() {
+	return () => {}
+}
